07877bdca122f43ac1be8a55c2e8596b41b6d3e4
Bankai07 <123294396+Bankai07@users.noreply.github.com>
2026-07-11 01:47:04 +0530

    Delete Navbar.tsx

--- Navbar.tsx
+++
@@ -1,4 +0,0 @@
-import Logo from "./Logo";
-import NavLinks from "./NavLinks";
-import CTAButton from "./CTAButton";
-export default function Navbar(){return <header style={{display:"flex",justifyContent:"space-between",padding:"20px 40px"}}><Logo/><NavLinks/><CTAButton/></header>;}
